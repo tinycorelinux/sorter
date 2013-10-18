6f7be24406dc2906f9c31d890ed2f38122b66276
Lauri Kasanen <curaga@operamail.com>
2013-10-18 17:55:40 +0300

    Use rm -f here

--- sorter.sh
+++ sorter.sh
@@ -129,7 +129,7 @@ mkdir tmp
 cd tmp
 tar xf ../base_modules.tgz
 depmod -a -b . ${KERNEL}
-rm lib/modules/${KERNEL}/*map
+rm -f lib/modules/${KERNEL}/*map
 rm lib/modules/${KERNEL}/modules.symbols
 find lib usr | cpio -o -H newc | gzip -9 > ../modules${is64}.gz
 
